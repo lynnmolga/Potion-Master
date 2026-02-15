d7070863453952d28dea1faacb78359c519c19c3
lynnmolga <118103291+lynnmolga@users.noreply.github.com>
2026-02-15 16:54:39 +0200

    cauldron path fix.js

--- src/main.js
+++ src/main.js
@@ -148,7 +148,7 @@ preload() {
   this.load.image("bg", asset("../assets/witch_room.png"));
 
   for (let i = 1; i <= 7; i++) {
-    this.load.image(`cauldron${i}`, asset(`../assets/cauldron${i}.png`));
+    this.load.image(`cauldron${i}`, asset(`../assets/cauldron${i}.PNG`));
   }
 }
 
